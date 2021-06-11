2c5a726e500a642fdaf175d5095441c0081ede90
Amanda-G10 <reahana233@gmail.com>
2021-06-11 11:15:11 +0600

    cgpa logic

--- js/CAL.js
+++ js/CAL.js
@@ -597,3 +597,30 @@ function grade8() {
 
     
 }//Function
+
+//FINAL
+function cgpa() {
+    
+
+var X1 = document.getElementById("1").value;
+var a1 = X1 * 18;
+var X2 = document.getElementById("2").value;
+var a2 = X2 * 16.5;
+var X3 = document.getElementById("3").value;
+var a3 = X3 * 18;
+var X4 = document.getElementById("4").value;
+var a4 = X4 * 16.5;
+var X5 = document.getElementById("5").value;
+var a5 = X5 * 16.5;
+var X6 = document.getElementById("6").value;
+var a6 = X6 * 16.5;
+var X7 = document.getElementById("7").value;
+var a7 = X7 * 18;
+var X8 = document.getElementById("8").value;
+var a8 = X8 * 18;
+
+var Cr = 138;
+var result = (a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8) / Cr;
+
+document.getElementById("demo").innerHTML = result;
+}
